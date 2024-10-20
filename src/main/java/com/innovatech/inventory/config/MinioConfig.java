f8285828145d9702644fce365ce99a9fe53b8903
Rinconjr <nicorinconb@yahoo.com>
2024-10-19 23:01:30 -0500

    .env working

--- src/main/java/com/innovatech/inventory/config/MinioConfig.java
+++ src/main/java/com/innovatech/inventory/config/MinioConfig.java
@@ -1,6 +1,8 @@
 package com.innovatech.inventory.config;
 
 import io.minio.MinioClient;
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +11,13 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class MinioConfig {
 
-    @Value("${minio.url}")
+    @Value("${minio.url:NOT_FOUND}")
     private String minioUrl;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("MinIO URL: " + minioUrl);
+    }
 
     @Value("${minio.access.key}")
     private String accessKey;
