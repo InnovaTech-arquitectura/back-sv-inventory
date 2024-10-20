f3901d48774faa80f38ebc3d0b0ad14946129374
Juan Camilo Quintana <jc.quintana@uniandes.edu.co>
2024-10-19 21:47:52 -0500

    cors y el propeties

--- src/main/java/com/innovatech/inventory/config/MinioConfig.java
+++ src/main/java/com/innovatech/inventory/config/MinioConfig.java
@@ -1,17 +1,27 @@
 package com.innovatech.inventory.config;
 
 import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioConfig {
 
+    @Value("${minio.url}")
+    private String minioUrl;
+
+    @Value("${minio.access.key}")
+    private String accessKey;
+
+    @Value("${minio.access.secret}")
+    private String secretKey;
+
     @Bean
     public MinioClient minioClient() {
         return MinioClient.builder()
-                .endpoint("https://your-minio-endpoint.com")
-                .credentials("your-access-key", "your-secret-key")
+                .endpoint(minioUrl) // Usar el valor correcto desde las propiedades
+                .credentials(accessKey, secretKey) // Usar credenciales desde las propiedades
                 .build();
     }
 }
