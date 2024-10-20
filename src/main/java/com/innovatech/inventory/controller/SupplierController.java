14c2b3be7cc4aff35458f4ec1c74561ac6b44bf3
Andrés García Montoya <61592522+andres345gm@users.noreply.github.com>
2024-10-20 09:22:33 -0500

    CRUD supplier

---
+++ src/main/java/com/innovatech/inventory/controller/SupplierController.java
@@ -0,0 +1,64 @@
+package com.innovatech.inventory.controller;
+
+import com.innovatech.inventory.dto.NewSupplierDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.innovatech.inventory.service.SupplierService;
+
+@RestController
+@RequestMapping("/supplier")
+public class SupplierController {
+    @Autowired
+    private SupplierService supplierService;
+
+    @GetMapping("/all")
+    public ResponseEntity<?> listSuppliers() {
+        try {
+            return ResponseEntity.ok(supplierService.listSuppliers());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error listing suppliers");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getSupplierById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(supplierService.getSupplierById(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Supplier not found");
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createSupplier(@RequestBody NewSupplierDTO newSupplierDTO) {
+        try {
+            return ResponseEntity.ok(supplierService.createSupplier(newSupplierDTO));
+        } catch (Exception e) {
+            //return ResponseEntity.badRequest().body("Error creating supplier");
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editSupplier(@PathVariable Long id, @RequestBody NewSupplierDTO newSupplierDTO) {
+        try {
+            if (supplierService.getSupplierById(id) == null) {
+                return ResponseEntity.badRequest().body("Supplier not found");
+            }
+            return ResponseEntity.ok(supplierService.editSupplier(id, newSupplierDTO));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error editing supplier");
+        }
+    }
+
+    @GetMapping("/product/{productId}")
+    public ResponseEntity<?> getSuppliersByProductId(@PathVariable Long productId) {
+        try {
+            return ResponseEntity.ok(supplierService.getSuppliersByProductId(productId));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error listing suppliers");
+        }
+    }
+
+}
