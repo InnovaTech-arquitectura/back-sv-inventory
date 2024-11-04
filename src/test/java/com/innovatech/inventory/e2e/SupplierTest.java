dcf678b375c14f224db6bb660dcb38ac989e7851
Alejandro Suarez <101528810+suaracost@users.noreply.github.com>
2024-11-03 22:15:13 -0500

    Selenium proveedores listo

--- src/test/java/com/innovatech/inventory/e2e/SupplierTest.java
+++ src/test/java/com/innovatech/inventory/e2e/SupplierTest.java
@@ -50,13 +50,55 @@ public class SupplierTest {
         WebElement inputPassword = driver.findElement(By.id("password"));
         WebElement inputUser = driver.findElement(By.id("tipoUser"));
 
-        inputMail.sendKeys("sanyaba03@hotmail.com");
-        inputPassword.sendKeys("123");
+        inputMail.sendKeys("prueba@mail.com");
+        inputPassword.sendKeys("1234");
         Select selectUser = new Select(inputUser);
         selectUser.selectByIndex(1);
 
         WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/app-inicio-sesion/div/div[2]/form/button")));
         loginButton.click();
+
+        // Navega a la sección de agregar proveedores
+        WebElement suppliersButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("linkSupplier")));
+        suppliersButton.click();
+
+        WebElement addSupplierButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("addSupplier")));
+        addSupplierButton.click();
+
+        // Ingresa los datos de un proveedor
+        WebElement inputName = wait.until(ExpectedConditions.elementToBeClickable(By.id("nombre")));
+        WebElement inputContact = wait.until(ExpectedConditions.elementToBeClickable(By.id("contacto")));
+        WebElement inputDesc = wait.until(ExpectedConditions.elementToBeClickable(By.id("descripcion")));
+
+        inputName.sendKeys("Proveedor 1");
+        inputContact.sendKeys("3213228515");
+        inputDesc.sendKeys("Proveedor de coca cola");
+
+        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/app-add-supplier/body/section/div[2]/div/a")));
+        saveButton.click();
+
+        // Edita el proveedor
+        WebElement editButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/app-all-suppliers/body/section/div[2]/table/tbody/tr/td[4]/a[1]")));
+        editButton.click();
+
+        WebElement inputDescEdit = wait.until(ExpectedConditions.elementToBeClickable(By.id("descripcion")));
+        inputDescEdit.sendKeys(" editado");
+
+        WebElement saveButtonEdit = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/app-info-supplier/body/section/div[2]/div[2]/button")));
+        saveButtonEdit.click();
+
+        WebElement confirmEdit = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/div/div[6]/button[1]")));
+        confirmEdit.click();
+
+        // Elimina el proveedor
+        WebElement deleteButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/app-all-suppliers/body/section/div[2]/table/tbody/tr/td[4]/a[2]")));
+        deleteButton.click();
+
+        WebElement confirmButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/div/div[6]/button[3]")));
+        confirmButton.click();
+
+        WebElement confirmDelete = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/div/div[6]/button[1]")));
+        confirmDelete.click();
     }
 
     @AfterEach
